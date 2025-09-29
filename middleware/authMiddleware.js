86ff82dbd84d4dbfd4205b2074ef0008ddffdb1b
Deathfile78 <140796731+Deathfile78@users.noreply.github.com>
2025-09-29 10:30:46 +0000

    done

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const verifySecret = async (req, res, next) =>{
     console.log(req.header("SHIPPING_SECRET_KEY"))
     if (!req.header("SHIPPING_SECRET_KEY")){
-        return res.status(403).json({"error": "SHIPPING_SECRET_KEY is missing or invalid"})
+        return res.status(403).json({"error": "apiauthkey is missing or invalid"})
     }
     if(req.header("SHIPPING_SECRET_KEY") != 'a1b2c3d4e5f67890123456789abcdef'){
         return res.status(403).json({"error": "Failed to authenticate SHIPPING_SECRET_KEY"})
